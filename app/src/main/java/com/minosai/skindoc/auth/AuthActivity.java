d8528efaaa27cfd2fdaff370241e908c0e21b41c
Yaswant Narayan <yoyaswant@gmail.com>
2017-12-29 00:04:54 +0530

    Created fragments for auth

--- app/src/main/java/com/minosai/skindoc/auth/AuthActivity.java
+++ app/src/main/java/com/minosai/skindoc/auth/AuthActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 
 import com.minosai.skindoc.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class AuthActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_auth);
+
+        getSupportActionBar().hide();
     }
 }
